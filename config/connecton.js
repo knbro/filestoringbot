67fc28a7f22528cac999f3ffa81354f76239cade
knbro <81792272+knbro@users.noreply.github.com>
2021-10-22 22:40:55 +0530

    Update connecton.js

--- config/connecton.js
+++ config/connecton.js
@@ -9,6 +9,13 @@ module.exports.connect = function (done) {
     url = process.env.DB_URL
     const dbname = 'mediaFile'
 
+saveUser: (user) => {
+        db.get().collection(collection.USER_COLLECTION).createIndex({ userId: 1 }, { unique: true })
+        db.get().collection(collection.USER_COLLECTION).insertOne(user).catch((err) => {
+            console.log('already existing user');
+        })
+    },
+
     MongoClient.connect(url,{ useUnifiedTopology: true } , (err, data) => {
         if (err) return done(err)
         state.db = data.db(dbname)
